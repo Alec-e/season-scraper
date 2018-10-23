e1f7578ed1bb1326b56035fe3bbface23185486b
Kevin Ullyott <ullyott.kevin@gmail.com>
2018-10-22 23:05:55 -0700

    Create database for games

---
+++ database/migrations/2018_10_23_055638_create_games_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('opponent');
+            $table->string('date');
+            $table->string('time');
+            $table->string('location');
+            $table->string('status');
+            $table->string('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
